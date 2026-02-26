1fe78467a8a354a44b7afeb7977a0471a90c3273
chandan thakur <stak7085@gmail.com>
2026-02-26 18:59:24 +0530

    chore(security): configure express body parser with 10mb payload limit

--- src/app.js
+++ src/app.js
@@ -5,4 +5,16 @@ const app = express();
 app.disable('x-powered-by');
 app.set('trust proxy', 1);
 
+app.use(
+  express.json({
+    limit: '10mb',
+  }),
+);
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '10mb',
+  }),
+);
+
 module.exports = app;
