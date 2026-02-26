720408aacbc954d46b4a9e5549796e473bc342e2
chandan thakur <stak7085@gmail.com>
2026-02-26 18:54:50 +0530

    chore(security): disable x-powered-by and configure trust proxy for production readiness

--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ const confg = require('./config/env');
 const express = require('express');
 const app = express();
 
+app.disable('x-powered-by');
+app.set('trust proxy', 1);
+
 module.exports = app;
